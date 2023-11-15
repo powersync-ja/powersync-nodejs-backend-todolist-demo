e511ea03e261d15ed29a1913704cb03682080ff1
Mike Barnes <michael@journeyapps.com>
2023-11-15 16:04:42 -0700

    Implemented the PowerSync data upload endpoints

--- src/api/data.js
+++ src/api/data.js
@@ -1,10 +1,10 @@
 import express from "express";
-import {Client} from "pg";
+import {Pool} from "pg";
 import config from "../../config.js";
 
 const router = express.Router();
 
-const client = new Client({
+const pool = new Pool({
     host: config.database.host,
     database: config.database.name,
     user: config.database.user,
@@ -12,31 +12,109 @@ const client = new Client({
     port: config.database.port,
 });
 
+pool.on('error', (err, client) => {
+    console.error("Pool connection failure to postgres:", err, client);
+})
+
 /**
  * Handle all PUT events sent to the server by the client PowerSunc application
  */
 router.put("/", async (req, res) => {
-    await client.connect();
+    if(!req.body) {
+        res.status(400).send({
+            message: "Invalid body provided"
+        });
+        return;
+    }
 
-    await client.end();
+    await upsert(req.body, res);
 });
 
 /**
  * Handle all PATCH events sent to the server by the client PowerSunc application
  */
 router.patch("/", async (req, res) => {
-    await client.connect();
+    if(!req.body) {
+        res.status(400).send({
+            message: "Invalid body provided"
+        });
+        return;
+    }
 
-    await client.end();
+    await upsert(req.body, res);
 });
 
 /**
  * Handle all DELETE events sent to the server by the client PowerSunc application
  */
 router.delete("/", async (req, res) => {
-    await client.connect();
+    if(!req.body) {
+        res.status(400).send({
+            message: "Invalid body provided"
+        });
+        return;
+    }
 
-    await client.end();
+    const table = req.body.table;
+    const data = req.body.data;
+
+    let text = null;
+    const values = [data.id];
+
+    switch(table) {
+        case 'lists':
+            text = 'DELETE FROM lists WHERE id = $1';
+            break;
+        case 'todos':
+            text = 'DELETE FROM todos WHERE id = $1';
+            break;
+        default:
+            break;
+    }
+
+    const client = await pool.connect();
+    await client.query(text, values);
+    await client.release();
+    res.status(200).send({
+        message: `PUT completed for ${table} ${data.id}`
+    })
+
+    res.status(400).send({
+        message: "Invalid body provided, expected table and data"
+    });
 });
+
+const upsert = async (body, res) => {
+    const table = body.table;
+    const data = body.data;
+
+    let text = null;
+    let values = [];
+
+    switch(table) {
+        case 'lists':
+            text = 'INSERT INTO lists(id, created_at, name, owner_id) VALUES ($1, $2, $3, $4) ON CONFLICT (id) DO UPDATE SET created_at = EXCLUDED.created_at, name = EXCLUDED.name, owner_id = EXCLUDED.owner_id';
+            values = [data.id, data.created_at, data.name, data.owner_id];
+            break;
+        case 'todos':
+            text = 'INSERT INTO todos(id, created_at, completed_at, description, completed, created_by, completed_by, list_id) VALUES ($1, $2, $3, $4, $5, $6, $7, $8) ON CONFLICT (id) DO UPDATE SET created_at = EXCLUDED.created_at, completed_at = EXCLUDED.completed_at, description = EXCLUDED.description, completed = EXCLUDED.completed, created_by = EXCLUDED.created_by, completed_by = EXCLUDED.completed_by, list_id = EXCLUDED.list_id';
+            values = [data.id, data.created_at, data.completed_at, data.description, data.completed, data.created_by, data.completed_by, data.list_id];
+            break;
+        default:
+            break;
+    }
+    if(text && values.length > 0) {
+        const client = await pool.connect();
+        await client.query(text, values);
+        await client.release();
+        res.status(200).send({
+            message: `PUT completed for ${table} ${data.id}`
+        })
+    } else {
+        res.status(400).send({
+            message: "Invalid body provided, expected table and data"
+        });
+    }
+}
 
 export { router as dataRouter };
