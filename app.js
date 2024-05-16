a1e2cf294537e9267d47904daf4b5edd025a3677
Steven Ontong <steven@journeyapps.com>
2024-05-16 13:17:54 +0200

    add formatting

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-import express from "express";
-import bodyParser from "body-parser";
-import {apiRouter} from "./src/api/index.js";
-import logRequest from "./src/middleware/logger.js";
-import config from "./config.js";
+import express from 'express';
+import bodyParser from 'body-parser';
+import { apiRouter } from './src/api/index.js';
+import logRequest from './src/middleware/logger.js';
+import config from './config.js';
 
 const app = express();
 
@@ -10,33 +10,27 @@ app.use(bodyParser.json());
 app.use(logRequest);
 
 app.use((req, res, next) => {
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', '*');
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', '*');
 
-    // Request methods you wish to allow
-    res.setHeader(
-        'Access-Control-Allow-Methods',
-        'GET, POST, OPTIONS, PUT, PATCH, DELETE'
-    );
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
-    // Request headers you wish to allow
-    res.setHeader(
-        'Access-Control-Allow-Headers',
-        'X-Requested-With,content-type'
-    );
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
 
-    // Pass to next layer of middleware
-    next();
+  // Pass to next layer of middleware
+  next();
 });
 try {
-    app.get('/', (req, res) => {
-        res.status(200).send({
-            message: "powersync-nodejs-backend-todolist-demo"
-        });
+  app.get('/', (req, res) => {
+    res.status(200).send({
+      message: 'powersync-nodejs-backend-todolist-demo'
     });
-    app.use('/api', apiRouter);
+  });
+  app.use('/api', apiRouter);
 } catch (err) {
-    console.log("Unexpected error", err);
+  console.log('Unexpected error', err);
 }
 
 export default app;
--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-import app from "./app.js";
-import config from "./config.js";
+import app from './app.js';
+import config from './config.js';
 
 const PORT = process.env.PORT || config.port;
 
 app.listen(PORT, () => {
-    console.log(`Server is running @ http://127.0.0.1:${PORT}`);
+  console.log(`Server is running @ http://127.0.0.1:${PORT}`);
 });
